cdceda06605c07c47c24af7453de14685ad3597d
zamsaalbertdaniel <danielzamsaa@gmail.com>
2025-09-11 00:30:38 +0300

    app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{color:'#fff',padding:24}}>
+      <h1>Generator Loto AI</h1>
+      <p>Aici vei pune App.tsx real din proiectul tău.</p>
+    </div>
+  )
+}
